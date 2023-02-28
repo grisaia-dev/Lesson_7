66c21e3dc33b6b173ee2f466264aad12ead72d48
JateX <grisaia@inbox.ru>
2023-02-28 06:16:44 +0300

    Task_2

---
+++ Task_2/Task_2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+#define SUB(_a, _b) ((_a) - (_b))
+
+int main() {
+    int a = 6, b = 5, c = 2;
+
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+}
